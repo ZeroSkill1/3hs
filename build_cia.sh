633ff052d83775825528fef3c117a7a586a33e73
TimmSkiller <timmtimm1234567890@gmail.com>
2021-03-17 19:47:48 +0300

    fre

---
+++ build_cia.sh
@@ -0,0 +1,6 @@
+make RELEASE=1
+3dstool -ctf romfs cia_stuff/romfs.bin --romfs-dir romfs
+makerom -f ncch -o 3hs.cxi -target t -elf 3hs.elf -icon cia_stuff/icon.smdh -banner cia_stuff/banner.bnr -rsf cia_stuff/3hs.rsf -romfs cia_stuff/romfs.bin
+makerom -f cia -o 3hs.cia -i 3hs.cxi:0:0 -ignoresign
+
+
