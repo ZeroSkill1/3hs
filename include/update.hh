ee15e6d02431ce4689e76430dc5a9b53396be9ca
MyPasswordIsWeak <MyPasswordIsWeak@protonmail.com>
2021-11-30 16:11:21 +0100

    bump version

--- include/update.hh
+++ include/update.hh
@@ -17,8 +17,8 @@
 #endif
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 1
-#define VERSION_PATCH 6
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
 
 #define INT_TO_STR(i) INT_TO_STR_(i)
 #define INT_TO_STR_(i) #i
